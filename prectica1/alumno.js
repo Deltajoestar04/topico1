e2a69d618dd31a39eb7bc4b370237c0899fe1f01
Deltajoestar04 <jupreciado808@gmail.com>
2025-08-30 12:41:01 -0600

    sii

--- prectica1/alumno.js
+++ prectica1/alumno.js
@@ -10,15 +10,12 @@ class Alumno {
         this.apellido = apellido;
         this.correo = correo;
     }
-
-
     obtenerNombreCompleto() {
         return `${this.nombre} ${this.apellido}`;
     }
 }
 const ModuloAlumnos = (() => {
 let alumnosInscritos = [];
-
 function ingresarAlumno(alumno) {
         alumnosInscritos.push(alumno);
         console.log(`Alumno ingresado: ${alumno.obtenerNombreCompleto()}`);
@@ -28,19 +25,16 @@ function darDeBajaAlumno(correo) {
         alumnosInscritos = alumnosInscritos.filter(a => a.correo !== correo);
         console.log(`Alumno con correo ${correo} fue dado de baja.`);
     }
-
 function listarAlumnos() {
         console.log("Lista de alumnos inscritos:");
         alumnosInscritos.forEach((a, index) => {
             console.log(`${index + 1}. ${a.obtenerNombreCompleto()} (${a.correo})`);
         });
     }
-
 function enviarCorreoID(alumno) {
         const id = `${alumno.obtenerNombreCompleto()} - ${listaDeTemas[0]}`;
         console.log(`Enviando correo... ID = ${id}`);
     }
-
 return {
         ingresarAlumno,
         darDeBajaAlumno,
@@ -51,14 +45,9 @@ return {
 
 const alumno1 = new Alumno("Ana", "Pérez", "ana.perez@gmail.com");
 const alumno2 = new Alumno("Luis", "García", "luis.garciagemail.com");
-
 ModuloAlumnos.ingresarAlumno(alumno1);
 ModuloAlumnos.ingresarAlumno(alumno2);
-
 ModuloAlumnos.listarAlumnos();
-
 ModuloAlumnos.enviarCorreoID(alumno1);
-
 ModuloAlumnos.darDeBajaAlumno("luis.garcia@gmail.com");
-
 ModuloAlumnos.listarAlumnos();
